ff04e70888392cd5393af61bbc4bf24d19fc4665
lyquochuy1988 <lyquochuy1988@gmail.com>
2025-01-04 07:25:06 +0700

    Bai 11 - Phim the gioi lap trinh

---
+++ bai11-phimthegioilaptrinh/index.php
@@ -0,0 +1,11 @@
+<?php 
+    $yourAge = 50;
+
+    if ($yourAge > 0 && $yourAge < 100) {
+        if ($yourAge >= 15) {
+            echo "Ban duoc xem phim The gioi lap trinh";
+        } else {
+            echo "Ban KHONG duoc xem phim The gioi lap trinh";
+        }
+    }
+?>
