48c8da4cbcff235a20f4ecfda79c8f7452a08d17
lyquochuy1988 <lyquochuy1988@gmail.com>
2025-01-07 09:02:13 +0700

    Bai 15 - Bang cuu chuong

---
+++ bai15-bangcuuchuong/index.php
@@ -0,0 +1,18 @@
+<?php 
+    // C1
+    // $number = 8;
+
+    // for ($i = 1; $i <= 10; $i++) {
+    //     echo $i . " x " . $number . " = " . ($i * $number) . "\n";
+    // }
+
+    // C2
+    $number = 8;
+    $result = "";
+
+    for ($i = 1; $i <= 10; $i++) {
+        $result .=  $i . " x " . $number . " = " . ($i * $number) . "\n";
+    }
+
+    echo $result;
+?>
